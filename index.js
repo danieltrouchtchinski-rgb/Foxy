82c64d98f0d0f7113cb75c6d7badcad86b45ad06
danieltrouchtchinski-rgb <danieltrouchtchinski@gmail.com>
2026-02-20 18:39:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ async function getQuote(symbol) {
 }
 
 // --- CHECK MARKETS ---
-console.log(symbol, "PRICE:", price, "OLD:", lastPrices[symbol]);
 console.log("FINNHUB KEY:", FINNHUB_KEY);
 async function checkMarkets() {
     try {
